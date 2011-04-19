32ce39d91bfc93c7f0f179d78fb4370a508a6a9f
Deke Brown <dknbrown@gmail.com>
2011-04-19 10:20:31 -0600

    finished chapt 5 review

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,21 @@
 class PagesController < ApplicationController
   def home
-  @title = "Home" 
+  @title = "Home"
+  @logo = "logo.png" 
   end
 
   def contact
   @title = "Contact" 
+  @logo = "logo.png"
   end
   
   def about
   @title = "About" 
+  @logo = "logo.png"
   end
 
   def help
   @title = "Help"
+  @logo = "logo.png"
   end
 end
